6f3a435fc9ce36a970e9960eca8f4ffd2e76b380
pantherinblack <stupark@bzz.ch>
2023-02-27 07:45:00 +0100

    created DeviceService, closes #6

---
+++ src/main/kotlin/ch/ksh/iotapi/service/DeviceService.kt
@@ -0,0 +1,27 @@
+package ch.ksh.iotapi.service
+
+import ch.ksh.iotapi.handler.DeviceHandler
+import ch.ksh.iotapi.model.Device
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/device")
+class DeviceService {
+    @ResponseBody
+    @GetMapping("/list")
+    fun listDevices(): ArrayList<Device> {
+        return DeviceHandler.getInstance().getDeviceList()
+    }
+
+    @ResponseBody
+    @GetMapping("/get")
+    fun getDeviceByUUID(
+        @RequestParam("uuid") uuid : String
+    ): Device? {
+        return DeviceHandler.getInstance().getDeviceByUUID(uuid)
+    }
+}
